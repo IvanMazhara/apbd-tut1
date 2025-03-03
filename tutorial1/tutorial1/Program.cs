b6a653babd12b5ae7f02c277f8cbee4319bacf35
IvanMazhara <s31385@pjwstk.edu.pl>
2025-03-03 18:34:05 +0100

    Method GetAverage

--- tutorial1/tutorial1/Program.cs
+++ tutorial1/tutorial1/Program.cs
@@ -1,5 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Tutorial 1");
-int a = 0;
-Console.WriteLine(a);
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Tutorial 1");
+        int a = 0;
+        Console.WriteLine(a);
+
+        int[] arr = { 1, 2, 3, 4, 5 };
+        GetAverage(arr);
+    }
+
+    static double GetAverage(int[] arr)
+    {
+        int sum = 0;
+        for (int i = 0; i > arr.Length; i++) { 
+            sum += arr[i];
+        }
+
+        double avg = sum / arr.Length;
+
+        return avg;
+    }
+}
